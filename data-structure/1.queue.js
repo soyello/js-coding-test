349e0bfa20ec52465379ac8bf635227b9e45cd01
soyello <wodus9193@naver.com>
2025-02-16 00:53:24 +0900

    기초 문법 및 핵심 자료구조

---
+++ data-structure/1.queue.js
@@ -0,0 +1,33 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
+  }
+  enqueue(item) {
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+  dequeue() {
+    const item = this.items[this.headIndex];
+    delete this.items[this.headIndex];
+    this.headIndex++;
+    return item;
+  }
+  peek() {
+    return this.items[this.headIndex];
+  }
+  getLength() {
+    return this.tailIndex - this.headIndex;
+  }
+}
+
+let queue = new Queue();
+
+queue.enqueue(5);
+queue.enqueue(2);
+queue.dequeue();
+
+while (queue.getLength() != 0) {
+  console.log(queue.dequeue());
+}
