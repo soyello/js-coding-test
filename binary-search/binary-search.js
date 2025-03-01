1b2750231b68b149951c7ba35f94d9f28d4f64ab
soyello <wodus9193@naver.com>
2025-03-02 08:36:32 +0900

    이진탐색

---
+++ binary-search/binary-search.js
@@ -0,0 +1,285 @@
+//1. 재귀함수
+
+// function binarySearch(arr, target, start, end) {
+//   if (start > end) {
+//     return -1;
+//   }
+//   let mid = parseInt((start + end) / 2);
+//   if (arr[mid] == target) {
+//     return mid;
+//   } else if (arr[mid] > target) {
+//     return binarySearch(arr, target, start, mid - 1);
+//   } else {
+//     return binarySearch(arr, target, mid + 1, end);
+//   }
+// }
+
+// let n = 10;
+// let target = 7;
+// arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 10];
+
+// let result = binarySearch(arr, target, 0, n - 1);
+// if (result == -1) {
+//   console.log('원소가 존재하지 않습니다.');
+// } else {
+//   console.log(`${result + 1}번째 원소입니다.`);
+// }
+
+//2. 반복문
+
+// function binarySearch(arr, target, start, end) {
+//   while (start <= end) {
+//     let mid = parseInt((start + end) / 2);
+//     if (arr[mid] == target) {
+//       return mid;
+//     } else if (arr[mid] > target) {
+//       end = mid - 1;
+//     } else {
+//       start = mid + 1;
+//     }
+//     return -1;
+//   }
+//   let n = 10;
+//   let target = 7;
+//   arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+//   let result = binarySearch(arr, target, 0, n - 1);
+//   if (result == -1) {
+//     console.log('원소가 존재하지 않습니다.');
+//   } else {
+//     console.log(`${result + 1}번째 원소입니다.`);
+//   }
+// }
+
+//3.정렬된 배열에서 특정 원소의 개수 구하기
+// function lowerBound(arr, target, start, end) {
+//   while (start < end) {
+//     let mid = parseInt((start + end) / 2);
+//     if (arr[mid] >= target) {
+//       end = mid;
+//     } else {
+//       start = mid + 1;
+//     }
+//     return end;
+//   }
+// }
+
+// function upperBound(arr, target, start, end) {
+//   while (start < end) {
+//     let mid = parseInt((start + end) / 2);
+//     if (arr[mid] > target) {
+//       end = mid;
+//     } else {
+//       start = mid + 1;
+//     }
+//     return end;
+//   }
+// }
+
+// function countByRange(arr, leftValue, rightValue) {
+//   let rightIndex = upperBound(arr, rightValue, 0, arr.length);
+//   let leftIndex = lowerBound(arr, leftValue, 0, arr.length);
+//   return rightIndex - leftIndex;
+// }
+
+// let arr = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9];
+// console.log(countByRange(arr, 4, 4)); //값이 4인 데이터 출력
+// console.log(countByRange(arr, -1, 3)); //[-1,3] 범위에 있는 데이터 개수 출력
+
+//4.파라메트릭 서치 이해하기
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let n = Number(input[0]);
+// let arr = input[1].split(' ').map(Number);
+// let m = Number(input[2]);
+
+// let start = 1;
+// let end = arr.reduce((a, b) => Math.max(a, b));
+
+// let result = 0;
+// while (start <= end) {
+//   let mid = parseInt((start + end) / 2);
+//   let total = 0;
+//   for (let x of arr) {
+//     total += Math.min(mid, x);
+//   }
+//   if (total <= m) {
+//     result = mid;
+//     start = mid + 1;
+//   } else {
+//     end = mid - 1;
+//   }
+// }
+// console.log(result);
+
+//5. 나무 자르기
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let [N, M] = input[0].split(' ').map(Number);
+// let arr = input[1].split(' ').map(Number);
+
+// let start = 0;
+// let end = arr.reduce((a, b) => Math.max(a, b));
+
+// let answer = 0;
+// while (start <= end) {
+//   let mid = parseInt((start + end) / 2);
+//   let total = 0;
+//   for (let x of arr) {
+//     if (x > mid) {
+//       total += x - mid;
+//     }
+//   }
+//   if (total >= M) {
+//     answer = mid;
+//     start = mid + 1;
+//   } else {
+//     end = mid - 1;
+//   }
+// }
+// console.log(answer);
+
+// 6. 랜선 자르기
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let [K, N] = input[0].split(' ').map(Number);
+// let arr = input.slice(1, K + 1).map(Number);
+
+// let start = 1;
+// let end = arr.reduce((a, b) => Math.max(a, b));
+
+// let answer = 0;
+// while (start <= end) {
+//   let mid = parseInt((start + end) / 2);
+//   let total = 0;
+//   for (let x of arr) {
+//     total += Math.floor(x / mid);
+//   }
+//   if (total >= N) {
+//     answer = mid;
+//     start = mid + 1;
+//   } else {
+//     end = mid - 1;
+//   }
+// }
+
+// console.log(answer);
+
+//7. 숫자 카드
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// function lowerBound(arr, target, start, end) {
+//   while (start < end) {
+//     let mid = Math.floor((start + end) / 2);
+//     if (arr[mid] >= target) {
+//       end = mid;
+//     } else {
+//       start = mid + 1;
+//     }
+//   }
+//   return end;
+// }
+
+// function upperBound(arr, target, start, end) {
+//   while (start < end) {
+//     let mid = Math.floor((start + end) / 2);
+//     if (arr[mid] > target) {
+//       end = mid;
+//     } else {
+//       start = mid + 1;
+//     }
+//   }
+//   return end;
+// }
+
+// function countByRange(arr, leftValue, rightValue) {
+//   let leftIndex = lowerBound(arr, leftValue, 0, arr.length);
+//   let rightIndex = upperBound(arr, rightValue, 0, arr.length);
+//   return rightIndex - leftIndex;
+// }
+
+// let N = Number(input[0]);
+// let M = Number(input[2]);
+
+// let cards = input[1].split(' ').map(Number);
+// let numbers = input[3].split(' ').map(Number);
+
+// cards.sort((a, b) => a - b);
+
+// let answer = '';
+// for (let x of numbers) {
+//   let count = countByRange(cards, x, x);
+//   answer += count + ' ';
+// }
+
+// console.log(answer);
+
+//8. 병사 배치하기
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// function lowerBound(arr, target, start, end) {
+//   while (start < end) {
+//     let mid = Math.floor((start + end) / 2);
+//     if (arr[mid] >= target) {
+//       end = mid;
+//     } else {
+//       start = mid + 1;
+//     }
+//   }
+//   return end;
+// }
+
+// let N = Number(input[0]);
+// let arr = input[1].split(' ').map(Number);
+
+// arr.reverse();
+
+// let d = [0];
+// for (let x of arr) {
+//   if (d[d.length - 1] < x) {
+//     d.push(x);
+//   } else {
+//     let index = lowerBound(d,x,0,d.length);
+//     d[index] = x;
+//   }
+// }
+
+// console.log(N-(d.length-1))
+
+//9.K번째 수
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let N = Number(input[0]);
+let k = Number(input[1]);
+
+let start = 1;
+let end = 10 ** 10;
+
+let result = 0;
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
+  let total = 0;
+  for (let i = 1; i <= N; i++) {
+    total += Math.min(parseInt(mid / i), N);
+  }
+  if (total >= k) {
+    end = mid - 1;
+    result = mid;
+  } else {
+    start = mid + 1;
+  }
+}
+
+console.log(result);
--- greedy/greedy.js
+++ greedy/greedy.js
@@ -123,28 +123,336 @@
 
 //7. 신입 사원
 
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let testCase = Number(input[0]);
+// let line = 1;
+
+// for (let tc = 0; tc < testCase; tc++) {
+//   let n = Number(input[line]);
+//   let arr = [];
+//   for (let i = line + 1; i <= line + n; i++) {
+//     let data = input[i].split(' ').map(Number);
+//     arr.push(data);
+//   }
+
+//   arr.sort((a, b) => a[0] - b[0]);
+//   let minValue = 100001;
+//   let count = 0;
+//   for (let [x, y] of arr) {
+//     if (y < minValue) {
+//       minValue = y;
+//       count += 1;
+//     }
+//   }
+//   console.log(count);
+//   line += n + 1;
+// }
+
+//8-1.주유소
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let N = Number(input[0]);
+// let length = input[1].split(' ').map(Number);
+// let cost = input[2].split(' ').map(Number);
+
+// let total = BigInt(cost[0] * length[0]);
+// let minCost = cost[0];
+
+// for (let i = 1; i < N - 1; i++) {
+//   if (cost[i] < minCost) {
+//     total += BigInt(cost[i]) * BigInt(length[i]);
+//     minCost = cost[i];
+//   } else {
+//     total += BigInt(minCost) * BigInt(length[i]);
+//   }
+// }
+// console.log(String(total));
+
+//8-2. 주유소
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let n = Number(input[0]);
+// let dist = input[1].split(' ').map(Number);
+// let cost = input[2].split(' ').map(Number);
+
+// let total = BigInt(0);
+// let minCost = cost[0];
+// for (let i = 0; i < n; i++) {
+//   cost[i] = Math.min(cost[i], minCost);
+//   minCost = cost[i];
+// }
+
+// for (let i = 0; i < n - 1; i++) {
+//   total += BigInt(cost[i]) * BigInt(dist[i]);
+// }
+
+// console.log(String(total));
+
+//9. 회의실 배정
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let n = Number(input[0]);
+// let arr = [];
+
+// for (let i = 1; i <= n; i++) {
+//   arr.push(input[i].split(' ').map(Number));
+// }
+
+// arr.sort(function (a, b) {
+//   if (a[1] !== b[1]) {
+//     return a[1] - b[1];
+//   } else {
+//     return a[0] - b[0];
+//   }
+// });
+
+// let count = 1;
+// let cur = 0;
+// for (let i = 1; i < n; i++) {
+//   if (arr[cur][1] <= arr[i][0]) {
+//     count += 1;
+//     cur = i;
+//   }
+// }
+
+// console.log(count);
+
+//10. 풍선 맞추기
+
+// const rl = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// let input = [];
+// rl.on('line', function (line) {
+//   input.push(line);
+// }).on('close', function () {
+//   let data = input[1].split(' ').map(Number);
+//   let arrow = new Array(1000001).fill(0);
+//   let count = 0;
+//   for (let x of data) {
+//     if (arrow[x] > 0) {
+//       arrow[x] -= 1;
+//       arrow[x - 1] += 1;
+//     } else {
+//       arrow[x - 1] += 1;
+//       count += 1;
+//     }
+//   }
+//   console.log(count);
+//   process.exit();
+// });
+
+//11.피보나치 수열
+
+// const rl = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// let input = [];
+// rl.on('line', function (line) {
+//   input.push(line);
+// }).on('close', function () {
+//   let pibo = [];
+//   pibo.push(0);
+//   pibo.push(1);
+//   while (pibo[pibo.length - 1] < 1e9) {
+//     pibo.push(pibo[pibo.length - 1] + pibo[pibo.length - 2]);
+//   }
+
+//   let testCase = Number(input[0]);
+//   for (let tc = 1; tc <= testCase; tc++) {
+//     let n = Number(input[tc]);
+//     let result = [];
+
+//     let i = pibo.length - 1;
+//     while (n > 0) {
+//       if (n >= pibo[i]) {
+//         n -= pibo[i];
+//         result.push(pibo[i]);
+//       }
+//       i--;
+//     }
+
+//     let answer = '';
+//     for (let i = result.length - 1; i >= 0; i--) {
+//       answer += result[i] + ' ';
+//     }
+//     console.log(answer);
+//   }
+// });
+
+// 12.박 터뜨리기
+
+// const rl = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// let input = [];
+// rl.on('line', function (line) {
+//   input.push(line);
+// }).on('close', function () {
+//   let [N, K] = input[0].split(' ').map(Number);
+//   let sum = 0;
+//   for (let i = 1; i <= K; i++) {
+//     sum += i;
+//   }
+
+//   if(N<sum){
+//     console.log(-1);
+//   }else{
+//     N-=sum;
+//     if(N%K==0){
+//       console.log(K-1);
+//     }else{
+//       console.log(K);
+//     }
+//   }
+// });
+
+//13. 회문
+
+// const rl = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// let input = [];
+// rl.on('line', function (line) {
+//   input.push(line);
+// }).on('close', function () {
+//   function palindrome(x) {
+//     return x === x.split('').reverse().join('');
+//   }
+
+//   let testCases = Number(input[0]);
+
+//   for (let tc = 1; tc <= testCases; tc++) {
+//     let data = input[tc];
+//     let n = data.length;
+//     let ground = false;
+//     if (palindrome(data)) {
+//       console.log(0);
+//     } else {
+//       for (let i = 0; i < Math.floor(data.length / 2); i++) {
+//         if (data[i] !== data[n - i - 1]) {
+//           if (palindrome(data.slice(0, i) + data.slice(i + 1, n))) {
+//             ground = true;
+//           }
+//           if (palindrome(data.slice(0, n - i - 1) + data.slice(n - i, n))) {
+//             ground = true;
+//           }
+//           break;
+//         }
+//       }
+//       if (ground) {
+//         console.log(1);
+//       } else {
+//         console.log(2);
+//       }
+//     }
+//   }
+// });
+
+//14. 박스 채우기
+
+// const rl = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// let input = [];
+// rl.on('line', function (line) {
+//   input.push(line);
+// }).on('close', function () {
+//   function nearestSquare(x) {
+//     let i = 1;
+//     while (2 ** i <= x) {
+//       i += 1;
+//     }
+//     return i - 1;
+//   }
+
+//   let [length, width, height] = input[0].split(' ').map(Number);
+//   let cubes = new Array(20).fill(0);
+//   let n = Number(input[1]);
+//   for (let i = 2; i <= n + 1; i++) {
+//     let [a, b] = input[i].split(' ').map(Number);
+//     cubes[a] = b;
+//   }
+
+//   let size = 19;
+//   size = nearestSquare(length);
+//   size = Math.min(nearestSquare(width), size);
+//   size = Math.min(nearestSquare(height), size);
+
+//   let res = 0;
+//   let used = 0;
+
+//   for (let i = size; i >= 0; i--) {
+//     used *= 8;
+//     let cur = 2 ** i;
+//     let required = Math.floor(length / cur) * Math.floor(width / cur) * Math.floor(height / cur) - used;
+
+//     let usage = Math.min(required, cubes[i]);
+//     res += usage;
+//     used += usage;
+//   }
+//   if (used === length * width * height) {
+//     console.log(res);
+//   } else {
+//     console.log(-1);
+//   }
+// });
+
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let testCase = Number(input[0]);
-let line = 1;
+function nearestSquare(x) {
+  let i = 0;
+  while (2 ** i <= x) {
+    i++;
+  }
+  return i - 1;
+}
 
-for (let tc = 0; tc < testCase; tc++) {
-  let n = Number(input[line]);
-  let arr = [];
-  for (let i = line + 1; i <= line + n; i++) {
-    let data = input[i].split(' ').map(Number);
-    arr.push(data);
-  }
-  arr.sort((a, b) => a[0] - b[0]);
-  let count = 0;
-  let minValue = 100001;
-  for (let [x, y] of arr) {
-    if (y < minValue) {
-      count += 1;
-      minValue = y;
-    }
-  }
-  console.log(count);
-  line += n + 1;
+let [length, width, height] = input[0].split(' ').map(Number);
+let n = Number(input[1]);
+let cubes = new Array(20).fill(0);
+
+for (let i = 2; i < n + 2; i++) {
+  let [a, b] = input[i].split(' ').map(Number);
+  cubes[a] = b;
+}
+
+let size = nearestSquare(length);
+size = Math.min(nearestSquare(width), size);
+size = Math.min(nearestSquare(height), size);
+
+let res = 0;
+let used = 0;
+for (let i = size; i >= 0; i--) {
+  used *= 8;
+  let cur = 2 ** i;
+  let required = Math.floor(length / cur) * Math.floor(width / cur) * Math.floor(height / cur) - used;
+  let usage = Math.min(required, cubes[i]);
+
+  res += usage;
+  used += usage;
+}
+
+if (used === length * width * height) {
+  console.log(res);
+} else {
+  console.log(-1);
 }
